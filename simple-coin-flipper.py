8116206dfaa40f043adb39f3ed8773d684f5340e
MuharomRahZ <rahzaki05@gmail.com>
2022-07-08 22:29:26 +0800

    add update | 2-4 coins flipper

--- simple-coin-flipper.py
+++ simple-coin-flipper.py
@@ -5,13 +5,28 @@ coin = {
     2:"TAILS"
 }
 
+coin2 = {
+    1:"HEADS",
+    2:"TAILS"
+}
+
+coin3 = {
+    1:"HEADS",
+    2:"TAILS"
+}
+
+coin4 = {
+    1:"HEADS",
+    2:"TAILS"
+}
+
 def mainMenu():
     try:
-        print(f"==================================")
-        print(f"WELCOME TO THE SIMPLE COIN FLIPPER!")
-        print(f"This program will help you flip a coin and see if it's HEADS or TAILS")
-        print(f"---CHOOSE MENU---\n1. FLIP A COIN\n2. EXIT")
+        print(f"WELCOME TO THE SIMPLE COIN FLIPPER")
+        print(f"This program will help you flip a coin and see if it's heads or tails")
+        print(f"---CHOOSE MENU---\n1. FLIP A COIN\n2. FLIP TWO COINS\n3. FLIP THREE COINS\n4. FLIP FOUR COINS\n5. EXIT")
         menu = int(input("Enter your menu choice: "))
+        print()
         opsiMenu(menu)
         print()
     except Exception as exception:
@@ -21,10 +36,21 @@ def opsiMenu(menu):
     if menu == 1:
         coinFlipper()
     elif menu == 2:
-        print("Thank you for using the Simple Coin Flipper!")
+        coinFlipper2()
+    elif menu == 3:
+        coinFlipper3()
+    elif menu == 4:
+        coinFlipper4()
+    elif menu == 5:
+        print(f"============================================")
+        print(f"Thank you for using the Simple Coin Flipper!")
+        print(f"============================================")
         exit()
     else:
-        print("Invalid menu choice!")
+        print(f"=====!WARNING!======")
+        print(f"INVALID MENU CHOICE!")
+        print(f"====================")
+        print()
         mainMenu()
 
 def coinFlipper():
@@ -32,11 +58,62 @@ def coinFlipper():
     print(f"You have chosen to flip a coin")
     print(f"------------------------------")
     flipcoin = coin[rd.randint(1,2)]
-    print(f"---The coin has been flipped---")
-    print(f"|Results:                     |")
-    print(f"|You got {flipcoin}                |")
-    print(f"-------------------------------")
-    print(f"===============================")
+    print(f"|Results:                   |")
+    print(f"|           {flipcoin}           |")
+    print(f"------------------------------")
+    print(f"==============================")
+    print()
+    mainMenu()
+
+def coinFlipper2():
+    print(f"=================================")
+    print(f"You have chosen to flip two coins")
+    print(f"---------------------------------")
+    flipcoin = coin[rd.randint(1,2)]
+    flipcoin2 = coin2[rd.randint(1,2)]
+    print(f"|Results:                       |")
+    print(f"|     Coin 1    |     Coin 2    |")
+    print(f"---------------------------------")
+    print(f"|               |               |")
+    print(f"|     {flipcoin}     |     {flipcoin2}     |")
+    print(f"|               |               |")
+    print(f"=================================")
+    print()
+    mainMenu()
+
+
+def coinFlipper3():
+    print(f"==============================================")
+    print(f"      You have chosen to flip three coins     ")
+    print(f"----------------------------------------------")
+    flipcoin = coin[rd.randint(1,2)]
+    flipcoin2 = coin2[rd.randint(1,2)]
+    flipcoin3 = coin3[rd.randint(1,2)]
+    print(f"|Results:                                    |")
+    print(f"|     Coin 1    |    Coin 2    |    Coin 3   |")
+    print(f"----------------------------------------------")
+    print(f"|               |              |             |")
+    print(f"|     {flipcoin}     |     {flipcoin2}    |    {flipcoin3}    |")
+    print(f"|               |              |             |")
+    print(f"==============================================")
+    print()
+    mainMenu()
+
+def coinFlipper4():
+    print(f"===========================================================")
+    print(f"             You have chosen to flip four coins            ")
+    print(f"-----------------------------------------------------------")
+    flipcoin = coin[rd.randint(1,2)]
+    flipcoin2 = coin2[rd.randint(1,2)]
+    flipcoin3 = coin3[rd.randint(1,2)]
+    flipcoin4 = coin4[rd.randint(1,2)]
+    print(f"|Results:                                                 |")
+    print(f"|     Coin 1    |    Coin 2    |    Coin 3   |   Coin 4   |")
+    print(f"-----------------------------------------------------------")
+    print(f"|               |              |             |            |")
+    print(f"|     {flipcoin}     |     {flipcoin2}    |    {flipcoin3}    |    {flipcoin4}   |")
+    print(f"|               |              |             |            |")
+    print(f"===========================================================")
     print()
     mainMenu()
 
